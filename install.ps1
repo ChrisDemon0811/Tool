aacb66f38d6e073451c674988650b39de26fbe53
Lê Hồ Hoàng Long <2431540106@vaa.edu.vn>
2025-10-29 20:58:19 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,10 +1,10 @@
 $Url = "https://github.com/ChrisDemon0811/Tool/releases/download/v2/setup.exe"
 $Output = "$env:TEMP\setup.exe"
 
-Write-Host "Dang tai phan mem..."
+Write-Host "Đang tải phần mềm..."
 Invoke-WebRequest -Uri $Url -OutFile $Output
 
-Write-Host "Dang cai dat..."
+Write-Host "Đang cài đặt..."
 Start-Process -FilePath $Output -ArgumentList "/S" -Verb RunAs -Wait
 
-Write-Host "Cai dat hoan tat!"
+Write-Host "✅ Cài đặt hoàn tất!"
